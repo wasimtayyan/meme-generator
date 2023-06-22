19e375a92e5d58af78fd3fbe52324a857310de82
wasim altayyan <wasimtayyan@gmail.com>
2023-06-22 10:13:10 +0300

    feat
    add text shadow & font size

--- src/app/ImageMeme.jsx
+++ src/app/ImageMeme.jsx
@@ -1,53 +1,65 @@
 import React, { useState } from "react";
 import Image from "next/image";
 
-function ImageMeme({ imageUrl,topText,bottomText ,textColor}) {
-  const [count, setCount] = useState(0);
+function ImageMeme({
+  imageUrl,
+  topText,
+  bottomText,
+  textColor,
+  font,
+  fontSize,
+  textShadow,
+}) {
+  const [count, setCount] = useState(1);
   function handleClike() {
-    setCount((pre) => pre + 1);
-    
+    const imgIndex = Math.floor(Math.random() * imageUrl?.length);
+    setCount(imgIndex);
   }
   let data = imageUrl[count];
- 
-  
   return (
     <div className="">
-      {/* <div
-        className=""
-        style={{
-          backgroundImage: `url(${data?.url})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          height: data?.height,
-          width: data?.width,
-          position: "relative",
-        }}
-      >
-        <p style={{}}>top</p>
-        <p style={{ position: "absolute", top: data?.height - 100 }}>
-          dfjlsdfjsldfhjsldhjf;sodjhfsk;djfh
+      <div style={{ display: "inline-block", position: "relative" }}>
+        <img
+          style={{ width: "550px", height: "500px" }}
+          src={data?.url}
+          alt="MEME"
+        />
+        <p
+          style={{
+            position: "absolute",
+            top: "10%",
+            right: "40%",
+            color: textColor.top,
+            fontSize: `${fontSize}px`,
+            textShadow: textShadow ? "2px 2px 4px black" : "none",
+          }}
+          className={` font-medium ${font} `}
+        >
+          {topText}
         </p>
-      </div> */}
+        <p
+          style={{
+            position: "absolute",
+            bottom: "20%",
+            right: "40%",
 
-      <img
-        style={{ position: "relative" }}
-        height={500}
-        width={500}
-        src={data?.url}
-        alt="MEME"
-      />
-      <p style={{ position: "absolute", top: "10%" }}
-      className={`text-[${textColor.top}]`}
-      >{topText}</p>
-      <p style={{ position: "absolute", top: "70%" }}
-      className={`text-[${textColor.bottom}]`}
-      >{bottomText}</p>
-      <button
-        className="bg-teal-900 rounded-lg py-3 px-6 text-white text-lg my-5"
-        onClick={handleClike}
-      >
-        NEXT
-      </button>
+            color: textColor.bottom,
+            fontSize: `${fontSize}px`,
+            textShadow: textShadow ? "2px 2px 4px black" : "none",
+          }}
+          className={` font-medium ${font}`}
+        >
+          {bottomText}
+        </p>
+      </div>
+      <div>
+        <button
+          className="bg-teal-900 rounded-lg py-3 px-6 text-white text-lg my-5"
+          onClick={handleClike}
+        >
+          NEXT
+        </button>
+      </div>
     </div>
   );
 }
